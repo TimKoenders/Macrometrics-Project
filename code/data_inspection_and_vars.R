257224e431b9e0a0aa6aa86ec6280a9cea3c8540
Tim Koenders <koenders.tim@hotmail.com>
2023-06-09 18:26:29 +0200

    Data inspection + updated VAR / IRF estimation

--- code/data_inspection_and_vars.R
+++ code/data_inspection_and_vars.R
@@ -24,6 +24,23 @@ colnames(data)[7] <- "sp"
 data$month <- gsub("M", "", data$month) # Transforming col "month" into date format
 data$month <- ym(data$month, quiet = FALSE, tz = NULL, locale = Sys.getlocale("LC_TIME"))
 
+
+# Data inspection------------------------------------------------------------
+section_data <- data[data$month >= as.Date("2006-06-01") & data$month <= as.Date("2012-10-31"), ]
+summary(section_data$p_wheat)
+summary(section_data$p_wheat_2)
+summary(section_data$p_oil)
+summary(section_data$p_cotton)
+summary(section_data$reer)
+summary(section_data$ind_prod)
+summary(section_data$sp)
+summary(section_data$ir)
+summary(section_data$cpi)
+summary(section_data$netlong_mm)
+summary(section_data$netlong_swap)
+summary(section_data$exports_total)
+
+
 # Plotting time series ----------------------------------------------------
 plot(data$month, data$p_wheat, type = "l")
 plot(data$month, data$p_oil, type = "l")
@@ -57,54 +74,77 @@ merged_data<-data.frame(p_wheat_log_diff,p_oil_log_diff,reer_log,ind_prod_log_di
 
 d <- data$month[-1]
 merged_data<-data.frame(d,merged_data)
+
+
 #estimating VARs------------------------------
 install.packages("vars")
 
-### VAR for Money Managers###
-df_var_mm<-(data.frame(d,ind_prod_log_diff,exports_total_log_diff,netlong_mm_diff,p_wheat_log_diff))
-library(dplyr)
-df_var_mm$d<- as.Date(df_var_mm$d, format = "%Y-%m-%d")
-start_date <- as.Date("2006-07-01")
-end_date <- as.Date("2012-07-01")
-df_var_mm_final <- df_var_mm %>% filter(d >= start_date & d<= end_date)
-library(vars)
-is.na(df_var_mm_final)
-var_mm<- VAR(df_var_mm_final[,-1], lag.max = 10, ic = "AIC", type = "const")
-summary(var_mm)
-
-##VAR for Swap Dealers###
-df_var_sd<-(data.frame(d,ind_prod_log_diff,exports_total_log_diff,netlong_swap_diff,p_wheat_log_diff))
-library(dplyr)
-df_var_sd$d<- as.Date(df_var_mm$d, format = "%Y-%m-%d")
-start_date <- as.Date("2006-07-01")
-end_date <- as.Date("2012-07-01")
-df_var_sd_final <- df_var_sd %>% filter(d >= start_date & d<= end_date)
-library(vars)
-is.na(df_var_sd_final)
-var_sd<- VAR(df_var_sd_final[,-1], lag.max = 10, ic = "AIC", type = "const")
-summary(var_sd)
-
-###identify VAR and calculate impulse response functions###
-
+# Define bmat matrix 
 bmat <- matrix(0, nrow = 4, ncol = 4)
 bmat[lower.tri(bmat)] <- NA
 diag(bmat)<-NA
 
-### var_mm
-svar_mm <- SVAR(var_mm, estmethod = "direct", Bmat = bmat)
-n <- 10 
-ir_mm <- irf(svar_mm, n.ahead = n, cumulative = FALSE, boot = TRUE, ci = 0.9, runs = 300)
-plot(ir_mm)
+### VAR for Money Managers and Wheat###
+df_var_mm_w<-(data.frame(d,ind_prod_log_diff,exports_total_log_diff,netlong_mm_diff,p_wheat_log_diff))
+library(dplyr)
+df_var_mm_w$d<- as.Date(df_var_mm_w$d, format = "%Y-%m-%d")
+start_date <- as.Date("2006-07-01")
+end_date <- as.Date("2012-07-01")
+df_var_mm_w_final <- df_var_mm_w %>% filter(d >= start_date & d<= end_date)
+library(vars)
+is.na(df_var_mm_w_final)
+var_mm_w<- VAR(df_var_mm_w_final[,-1], lag.max = 10, ic = "AIC", type = "const")
+summary(var_mm_w)
+coefficients <- coef(var_mm_w)
+residuals <- resid(var_mm_w)
+print(coefficients)
 
-###var_sd
+# Computing impulse response functions with recursive-design wild bootstrap for var_mm_w
+set.seed(123) # for reproducibility
+nboot <- 1000 # number of bootstrap replications
+irf1 <- irf(var_mm_w, impulse = "ind_prod_log_diff", response = "p_wheat_log_diff", n.ahead = 10, boot = TRUE, nboot = nboot, ci = 0.95, boot.type = "rdwb")
+plot(irf1, ylim = c(-0.1,0.1), main="Demand Shock", ylab="Price of Wheat")
+irf2 <- irf(var_mm_w, impulse = "exports_total_log_diff", response = "p_wheat_log_diff", n.ahead = 10, boot = TRUE, nboot = nboot, ci = 0.95, boot.type = "rdwb")
+plot(irf2, ylim = c(-0.1,0.1), main="Supply Shock", ylab="Price of Wheat")
+irf3 <- irf(var_mm_w, impulse = "netlong_mm_diff", response = "p_wheat_log_diff", n.ahead = 10, boot = TRUE, nboot = nboot, ci = 0.95, boot.type = "rdwb")
+plot(irf3, ylim = c(-0.1,0.1), main="Financial Shock", ylab="Price of Wheat")
 
-svar_sd<- SVAR(var_sd, estmethod = "direct", Bmat = bmat)
-n <- 10 
-ir_sd <- irf(svar_sd, n.ahead = n, cumulative = FALSE, boot = TRUE, ci = 0.9, runs = 300)
-plot(ir_sd)
+
+##VAR for Swap Dealers and Wheat###
+df_var_sd_w<-(data.frame(d,ind_prod_log_diff,exports_total_log_diff,netlong_swap_diff,p_wheat_log_diff))
+library(dplyr)
+df_var_sd_w$d<- as.Date(df_var_sd_w$d, format = "%Y-%m-%d")
+start_date <- as.Date("2006-07-01")
+end_date <- as.Date("2012-07-01")
+df_var_sd_w_final <- df_var_sd_w %>% filter(d >= start_date & d<= end_date)
+library(vars)
+is.na(df_var_sd_w_final)
+var_sd_w<- VAR(df_var_sd_w_final[,-1], lag.max = 10, ic = "AIC", type = "const")
+summary(var_sd_w)
+coefficients <- coef(var_sd_w)
+residuals <- resid(var_sd_w)
+print(coefficients)
+
+# Computing impulse response functions with recursive-design wild bootstrap for var_sd_w
+set.seed(123) # for reproducibility
+nboot <- 1000 # number of bootstrap replications
+irf4 <- irf(var_sd_w, impulse = "ind_prod_log_diff", response = "p_wheat_log_diff", n.ahead = 10, boot = TRUE, nboot = nboot, ci = 0.95, boot.type = "rdwb")
+plot(irf4, ylim = c(-0.1,0.1), main="Demand Shock", ylab="Price of Wheat")
+irf5 <- irf(var_sd_w, impulse = "exports_total_log_diff", response = "p_wheat_log_diff", n.ahead = 10, boot = TRUE, nboot = nboot, ci = 0.95, boot.type = "rdwb")
+plot(irf5, ylim = c(-0.1,0.1), main="Supply Shock", ylab="Price of Wheat")
+irf6 <- irf(var_sd_w, impulse = "netlong_swap_diff", response = "p_wheat_log_diff", n.ahead = 10, boot = TRUE, nboot = nboot, ci = 0.95, boot.type = "rdwb")
+plot(irf6, ylim = c(-0.1,0.1), main="Financial Shock", ylab="Price of Wheat")
+
 
 ###forecast error variance decomposition
-fevd_mm<-fevd(svar_mm)
+fevd_mm<-fevd(var_mm_w)
 print(fevd_mm)
-fevd_sd<-fevd(svar_sd)
+fevd_sd<-fevd(var_sd_w)
 print(fevd_sd)
+
+
+
+
+
+
+
